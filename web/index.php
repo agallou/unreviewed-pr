01776a4df415405c1aa8890d3d0b0e297faa324d
Adrien Gallou <adriengallou@gmail.com>
2016-04-16 13:30:13 +0200

    add index and .htaccess

---
+++ web/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$container = require __DIR__ . "/../config/container.php";
+
+$app = new DI\Bridge\Silex\Application($containerBuilder);
+
+if (1 == $app['app.debug']) {
+    $app['debug'] = true;
+}
+
+$app->register(new \Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/capabilities', "controllers.hipchat.capabilities:action");
+$app->post('/installed', "controllers.hipchat.install:action");
+$app->get('/configure', "controllers.hipchat.configure:action");
+
+$app->get('/login_github', "controllers.github.login:action");
+$app->match('/github/webhook', 'controllers.github.webhook:action');
+
+$app->get('/app/list_repositories', "controllers.app.list_repositories:action")
+    ->before($app['middleware.needs_subscriber'])
+    ->before($app['middleware.needs_github_token'])
+;
+
+$app->run();
